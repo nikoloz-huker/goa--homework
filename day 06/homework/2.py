0d8f0d7dc6ffcad6a3802f5d101b9224f6aef7d5
nikoloz-huker <nikoloziedisherashvili14@gmail.com>
2024-11-15 23:36:31 +0400

    first commit

---
+++ day 06/homework/2.py
@@ -0,0 +1,8 @@
+name=input("please enter your name :")
+print(name)
+surname=input("please enter your surname :")
+print(surname)
+age=int(input("please enter your age :"))
+print(age)
+city=input("please enter your city :")
+print(city)
