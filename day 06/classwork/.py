0182e6b064614d10c0c6fb6a3f1841a8597143b4
nikoloz-huker <nikoloziedisherashvili14@gmail.com>
2024-11-19 21:53:59 +0400

    first commit

--- day 06/classwork/.py
+++ day 06/classwork/.py
@@ -1,1 +1,7 @@
-# დავალება არ მაქ
+name=input("please enter your name  :")
+
+surname=input("please enter your surname :")
+
+age= str(input("please enter your age :"))
+
+print(name + " " + surname + " "+  age)
