c5d9d6d3f0b904d61be037a9ad9b262b4f6ff9fd
nikoloz-huker <nikoloziedisherashvili14@gmail.com>
2024-12-22 16:43:53 +0400

    second commit

---
+++ day 12/classwork/.py
@@ -0,0 +1,16 @@
+
+n1=(10 < 11)
+n2=(20 > 30)
+n3=(30 < 40)
+n4=(40 > 50)
+n5=(50 < 60)
+n6=(10 > 11)
+n7=(20 < 30)
+n8=(30 > 40)
+n9=(40 < 50)
+n10=(50 > 60)
+print(n1 == n2) # false
+print(n3 == n4)  # false
+print(n5 == n6)  # false
+print(n7 == n8)  # false
+print(n9 == n10)  # false
---
+++ day 12/homework/.py
@@ -0,0 +1,12 @@
+a =(11)
+b = (12)
+c =(22)
+d =(88)
+e =(99)
+f =(1)
+g =(19)
+h =(31)
+print(a > b) # false
+print(c <= d) # true
+print(e == f) # false
+print(g < h) # True
