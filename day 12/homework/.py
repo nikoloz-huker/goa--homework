1df98d7d42eab50820866065fe5a58697944962e
nikoloz-huker <nikoloziedisherashvili14@gmail.com>
2024-12-23 21:12:02 +0400

    second commit

--- day 12/homework/.py
+++ day 12/homework/.py
@@ -6,7 +6,22 @@ e =(99)
 f =(1)
 g =(19)
 h =(31)
+i =(66)
+j =(30)
+k =(7)
+l =(77)
+m =(777)
+n =(0)
+o =(90)
+p =(17)
+q =(9)
+r =(37)
 print(a > b) # false
 print(c <= d) # true
 print(e == f) # false
 print(g < h) # True
+print(i != j) # true
+print(k > l) # false
+print(m < n ) # false
+print(o > p) # true
+print(q < r) # false
