feb475c1011d11ebecb8917bac6d6e25efa761a8
nikoloz-huker <nikoloziedisherashvili14@gmail.com>
2025-01-19 21:26:25 +0400

    second commit

---
+++ day 16/homework/3.py
@@ -0,0 +1,2 @@
+for n in range(50,150):
+   print(n)
---
+++ day 16/homework/6.py
@@ -0,0 +1,32 @@
+print(10 < 11) # true
+print(20 < 30) # true
+print(30 < 40) # true
+print(40 < 50) # true
+print(50 < 60) # true
+print(60 < 70) # true
+print(70 < 80) # true
+print(80 < 90) # true
+print(90 < 100) # true
+print(100 < 110) # true
+print(60 > 70) # false
+print(70 > 80) # false
+print(80 > 90) # false
+print(90 > 100) # false
+print(100 > 110)# false
+print(10 > 11) # false
+print(20 > 30) # false
+print(30 > 40) # false
+print(40 > 50) # false
+print(50 > 60) # false
+
+
+
+print(True and False) #false
+print(False and True) #false
+print(False and False) #false
+print(True and True) #true
+
+print(True or False) #true
+print(False or True) #true
+print(True or true) #true
+print(False or False) #false
